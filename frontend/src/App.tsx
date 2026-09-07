3d53d1c97c15e7a009afc99d6f9c157445d2be6c
Rza <rzamohammed404@gmail.com>
2026-09-07 22:23:44 +0530

    feat: initialize App component with React Router and route configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PipelinePage } from './pages/PipelinePage';
+import { NetworkStatus } from './components/NetworkStatus';
 
 export const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <NetworkStatus />
       <Routes>
         <Route path="/" element={<PipelinePage />} />
       </Routes>
