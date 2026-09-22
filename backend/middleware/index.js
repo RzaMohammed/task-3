5bc9f24733e5196196a1f07ece24c7637bcc407f
Rza <rzamohammed404@gmail.com>
2026-09-22 11:01:48 +0530

    feat(middleware): add security response headers middleware

--- backend/middleware/index.js
+++ backend/middleware/index.js
@@ -6,10 +6,13 @@ const errorHandler = require('./errorHandler');
 const rateLimit = require('./rateLimit');
 const corsMiddleware = require('./cors');
 const requestId = require('./requestId');
+const securityHeaders = require('./securityHeaders');
 
 module.exports = {
   ...errorHandler,
   rateLimit,
   corsMiddleware,
   requestId,
+  securityHeaders,
 };
+
